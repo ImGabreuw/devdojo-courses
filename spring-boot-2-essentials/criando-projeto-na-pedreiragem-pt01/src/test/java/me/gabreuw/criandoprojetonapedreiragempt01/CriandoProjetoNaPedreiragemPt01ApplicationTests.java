9364f5cd0e2760817a0610f07c08681a0ec8d0b2
ImGabreuw <imgabreuw.dev@gmail.com>
2021-03-11 18:10:51 -0300

    Criando projeto na pedreiragem pt. 01

---
+++ spring-boot-2-essentials/criando-projeto-na-pedreiragem-pt01/src/test/java/me/gabreuw/criandoprojetonapedreiragempt01/CriandoProjetoNaPedreiragemPt01ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.criandoprojetonapedreiragempt01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriandoProjetoNaPedreiragemPt01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
