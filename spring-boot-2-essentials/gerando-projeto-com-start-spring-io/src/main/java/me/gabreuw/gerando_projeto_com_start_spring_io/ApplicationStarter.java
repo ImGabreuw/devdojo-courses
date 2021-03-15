48dc2632de41166ec23c2a690aabdbb968978f2b
ImGabreuw <imgabreuw.dev@gmail.com>
2021-03-15 14:48:36 -0300

    Gerando projeto com start.spring.io

---
+++ spring-boot-2-essentials/gerando-projeto-com-start-spring-io/src/main/java/me/gabreuw/gerando_projeto_com_start_spring_io/ApplicationStarter.java
@@ -0,0 +1,13 @@
+package me.gabreuw.gerando_projeto_com_start_spring_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStarter.class, args);
+	}
+
+}
---
+++ spring-boot-2-essentials/gerando-projeto-com-start-spring-io/src/main/java/me/gabreuw/gerando_projeto_com_start_spring_io/domain/Anime.java
@@ -0,0 +1,14 @@
+package me.gabreuw.gerando_projeto_com_start_spring_io.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Anime {
+
+    private String name;
+
+}
