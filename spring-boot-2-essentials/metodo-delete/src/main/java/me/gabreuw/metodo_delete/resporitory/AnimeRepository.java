473b58cba1faac3258521e60b65e58d42b5903e5
ImGabreuw <imgabreuw.dev@gmail.com>
2021-03-17 16:46:51 -0300

    Spring Boot 2 Essentials 10 - Método DELETE

---
+++ spring-boot-2-essentials/metodo-delete/src/main/java/me/gabreuw/metodo_delete/resporitory/AnimeRepository.java
@@ -0,0 +1,9 @@
+package me.gabreuw.metodo_delete.resporitory;
+
+import me.gabreuw.metodo_delete.domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+    List<Anime> listAll();
+}
---
+++ spring-boot-2-essentials/metodo-delete/src/test/java/me/gabreuw/metodo_delete/ApplicationStarterTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.metodo_delete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
