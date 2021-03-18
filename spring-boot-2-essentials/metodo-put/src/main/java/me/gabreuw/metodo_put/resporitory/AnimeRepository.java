91d53203dde7b7df0e0a85a51886cdcb4ca8b516
ImGabreuw <imgabreuw.dev@gmail.com>
2021-03-18 17:13:34 -0300

    Método PUT

---
+++ spring-boot-2-essentials/metodo-put/src/main/java/me/gabreuw/metodo_put/resporitory/AnimeRepository.java
@@ -0,0 +1,9 @@
+package me.gabreuw.metodo_put.resporitory;
+
+import me.gabreuw.metodo_put.domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+    List<Anime> listAll();
+}
